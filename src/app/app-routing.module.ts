98a10984cf89e5887a63cd03a4fd5cf3cfbfe90f
trịnh đình thắng <trinhthang219@gmail.com>
2018-07-23 10:23:40 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'lazy', loadChildren: './lazy.module#LazyModule'},
+  { path: 'about', loadChildren: './about/about.module#AboutModule'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
